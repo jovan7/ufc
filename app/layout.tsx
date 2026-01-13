1a1ec83fe4b2ab069014f916508dd046aab4ab16
jovanjonic <jovanjonic@gmail.com>
2026-01-13 16:37:59 +0100

    base url

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const baseUrl = 'https://undergroundfightclub.com';
+const baseUrl = 'https://ufcpirot.netlify.app/';
 
 export const metadata: Metadata = {
     metadataBase: new URL(baseUrl),
